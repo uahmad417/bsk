bdaf3b493fd7dc74594333d9706ce714d390bfe5
uahmad417 <18u.ahmad@gmail.com>
2024-10-28 17:53:10 +0200

    US-5 tested the case for when there is a strike and a spare

--- test/test_bowling.py
+++ test/test_bowling.py
@@ -92,3 +92,18 @@ class TestBowlingGame(unittest.TestCase):
         game.add_frame(Frame(2, 6))
 
         self.assertEqual(94, game.calculate_score())
+
+    def test_calculate_score_strike_and_spare(self):
+        game = BowlingGame()
+        game.add_frame(Frame(10, 0))
+        game.add_frame(Frame(4, 6))
+        game.add_frame(Frame(7, 2))
+        game.add_frame(Frame(3, 6))
+        game.add_frame(Frame(4, 4))
+        game.add_frame(Frame(5, 3))
+        game.add_frame(Frame(3, 3))
+        game.add_frame(Frame(4, 5))
+        game.add_frame(Frame(8, 1))
+        game.add_frame(Frame(2, 6))
+
+        self.assertEqual(103, game.calculate_score())
