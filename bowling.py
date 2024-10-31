4fe32eb2d965b2264b85a9d8309eb1792c9ecf10
uahmad417 <18u.ahmad@gmail.com>
2024-10-31 11:17:40 +0200

    implemented the case where the last two frames are strikes

--- bowling.py
+++ bowling.py
@@ -27,15 +27,18 @@ class BowlingGame:
 
             if frame.is_spare():
                 if index == len(self.__frames)-1:
-                    frame.set_bonus(self.__bonus_throw)
+                    frame.set_bonus(self.__first_bonus_throw)
                 else:
                     frame.set_bonus(self.__frames[index+1].get_first_throw())
 
             if frame.is_strike():
                 if index == len(self.__frames)-1:
-                    frame.set_bonus(self.__bonus_throw + self.__second_bonus_throw)
+                    frame.set_bonus(self.__first_bonus_throw + self.__second_bonus_throw)
                 elif self.__frames[index+1].is_strike():
-                    frame.set_bonus(self.__frames[index+1].get_first_throw() + self.__frames[index+2].get_first_throw())
+                    if index == len(self.__frames) - 2:
+                        frame.set_bonus(self.__frames[index+1].get_first_throw() + self.__first_bonus_throw)
+                    else:
+                        frame.set_bonus(self.__frames[index+1].get_first_throw() + self.__frames[index+2].get_first_throw())
                 else:
                     frame.set_bonus(self.__frames[index+1].get_first_throw() + self.__frames[index+1].get_second_throw())
 
@@ -44,7 +47,7 @@ class BowlingGame:
         return score
 
     def set_first_bonus_throw(self, bonus_throw: int) -> None:
-        self.__bonus_throw = bonus_throw
+        self.__first_bonus_throw = bonus_throw
 
     def set_second_bonus_throw(self, bonus_throw: int) -> None:
         self.__second_bonus_throw = bonus_throw
--- test/test_bowling.py
+++ test/test_bowling.py
@@ -170,3 +170,20 @@ class TestBowlingGame(unittest.TestCase):
         game.set_second_bonus_throw(2)
 
         self.assertEqual(92, game.calculate_score())
+
+    def test_calculate_score_last_two_frame_strike(self):
+        game = BowlingGame()
+        game.add_frame(Frame(1, 5))
+        game.add_frame(Frame(3, 6))
+        game.add_frame(Frame(7, 2))
+        game.add_frame(Frame(3, 6))
+        game.add_frame(Frame(4, 4))
+        game.add_frame(Frame(5, 3))
+        game.add_frame(Frame(3, 3))
+        game.add_frame(Frame(4, 5))
+        game.add_frame(Frame(10, 0))
+        game.add_frame(Frame(10, 0))
+        game.set_first_bonus_throw(7)
+        game.set_second_bonus_throw(2)
+
+        self.assertEqual(110, game.calculate_score())
